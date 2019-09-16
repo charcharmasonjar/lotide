03355f0065acbcfdaa1a83a814126d480b0a6a50
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-16 22:06:04 +0000

    function that compares two values and prints out a success or failure message, complete with emojis

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🥳🥳🥳 Assertion Passed: ' + actual + ' === ' + expected);
+  } else if (actual !== expected) {
+    console.log('😬😬😬 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+//testing the function out
+
+assertEqual("Light", "House");
+assertEqual("light", "light");
+assertEqual(1, 1);
+assertEqual(2, 1);
