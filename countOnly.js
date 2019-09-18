84ef3cb4fb4d23baf64a82c5ac59597a67864de1
Charlotte Dibb <charlottedibb@Charlottes-MacBook-Air.local>
2019-09-18 12:50:49 -0700

    create function that takes in an array and an object, and returns an object countaing counts of items found in the input object

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+//function to test assertions
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😛 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+}
+
+//TEST CODE
+//array we're working on
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+//calling function, assigning it to a variable
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+//checking if it's working
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
