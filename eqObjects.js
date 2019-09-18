9f8c3babd5d5eee9bf6add24aca0fab9bac2c967
Charlotte Dibb <charlottedibb@Charlottes-MacBook-Air.local>
2019-09-18 15:22:57 -0700

    update function such that it works for objects that have primitive values

--- eqObjects.js
+++ eqObjects.js
@@ -7,6 +7,24 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+//check object have the same number of keys
+//check value of key in object2 is the same as object2
 const eqObjects = function(object1, object2) {
-
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for(const key in object1) {
+      if(object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
 };
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertEqual(eqObjects(ab, ba), true); // => true
+
+assertEqual(eqObjects(ab, abc), false); 
