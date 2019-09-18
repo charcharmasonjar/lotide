410580d879744ce5fb671401986449b25ac06f63
user956 <puneet_sran@icloud.com>
2019-09-18 03:09:41 +0000

    determining middle of arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,22 +10,11 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-<<<<<<< e0dea04c4ddc35bb740b9970e9b2898cd373bc08
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`😛 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`😰 Assertion Failed: ${actual} !== ${expected}`);
-=======
-const assertArraysEqual = function(array1, array2) {
-
-  let a1 = [1,2,3,4,5];
-  let a2 = [1,2,3];
-  if (eqArrays(a1, a2) === true) {
-    console.log(`\uD83D\uDC83 Assertion Passed: ${a1} === ${a2}`);
-  } else {
-    console.log(`\uD83D\uDE14 Assertion Failed: \"${a1}\" !== \"${a2}\"`);
->>>>>>> corrected assertArraysEqual function
   }
 };
 
