b4809369f0e661bb5622bbefb95204e505277c1e
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-18 13:04:36 -0700

    created function to count letters in a string and return and object of the letters and counts

---
+++ countLetters.js
@@ -0,0 +1,40 @@
+//code we will use to test the function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😛 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(stringToCount) {
+  const result = {};
+  for(const letter of stringToCount) {
+    if (letter !== ' ') {
+      if(result[letter]) {
+        result[letter] += 1
+      } else {
+        result[letter] = 1
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
+
+const result1 = (countLetters("lighthouse in the house"));
+const expected1 = {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+};
+
+assertEqual(result1, expected1);
