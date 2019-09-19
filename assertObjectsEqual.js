7ae9642367518a0a4293a8a450803dd2fdf25c39
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-18 17:47:19 -0700

    update function to display the object when logging the result to the console

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -29,13 +29,24 @@ const eqObjects = function (object1, object2) {
 };
 
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   if (eqObjects (actual, expected)) {
-    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅  Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`😰😰😰 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑  Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
+//test code
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertObjectsEqual(dc, cd2);
+
+assertObjectsEqual(ab, ba); // => true
+assertObjectsEqual(ab, abc); // => false
+assertObjectsEqual(cd, dc); // => true
+assertObjectsEqual(cd, cd2); // => false
+assertObjectsEqual(dc, cd2); // => false
