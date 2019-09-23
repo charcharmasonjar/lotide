626c518eb14492cc3bcd7c41b064e3552fd8dcf3
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-23 15:15:44 -0700

    incorporate chai assert into test for tail function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,26 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe('#tail', () => {
+  it('returns [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it('returns empty array for [‘5’]', () => {
+    assert.deepEqual(tail(['5']), []);
+  });
 
-// Test Case 2: Check the returned array elements
-const words2 = ["Hi"];
-tail(words2); // no need to capture the return value since we are not checking it
-assertEqual(words2.length, 1); // original array should still have 3 elements!
+  it('returns empty array when given an empty array', () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-const words3 = [];
-tail(words3); // no need to capture the return value since we are not checking it
-assertEqual(words3.length, 0); // original array should still have 3 elements!
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
+//tail - when given an array, returns every item except for the first
