20f933671c807ba328baf777a0c49dfceb8ee380
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-23 14:14:25 -0700

    create test for middle function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,7 +2,7 @@
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
+  if (eqArrays(actual, expected) === true) {
     console.log(`😛 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`😰 Assertion Failed: ${actual} !== ${expected}`);
