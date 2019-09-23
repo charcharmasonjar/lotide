b8c5922535f34168a37692eefe0b3fbedb22d21c
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-23 13:44:43 -0700

    create eqArrays test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`😰😰😰 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -19,8 +14,5 @@ const eqArrays = function (arr1, arr2) {
 };
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
+
--- tail.js
+++ tail.js
@@ -6,22 +6,4 @@ const tail = function(arr1) {
   return newArr;
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 2: Check the returned array elements
-const words2 = ["Hi"];
-tail(words2); // no need to capture the return value since we are not checking it
-assertEqual(words2.length, 1); // original array should still have 3 elements!
-
-const words3 = [];
-tail(words3); // no need to capture the return value since we are not checking it
-assertEqual(words3.length, 0); // original array should still have 3 elements!
+module.exports = tail;
