99cd0fe55d15fcd77b9871158aaa04c19a78fe3a
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-23 13:51:34 -0700

    create test file for assert arrays equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,5 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  }
-  return true;
-};
+
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -18,9 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [1, 2, 1]); // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-//how to get it to return the original array (to show if something is a string etc)
+module.exports = assertArraysEqual;
