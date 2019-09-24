a6781ccf387dd34549044033ca749eee37dbcda0
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-23 20:18:37 -0700

    update eqObjects to work on objects within objects

--- eqObjects.js
+++ eqObjects.js
@@ -10,10 +10,11 @@ const eqObjects = function (object1, object2) {
   }
   for (const key in object1) {
     if (Array.isArray(object1[key])) {
-      let isEqual = eqArrays(object1[key], object2[key]);
-      if (!isEqual) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+    } else if ((typeof object1[key]) === 'object') {
+      return eqObjects(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
       return false;
     }
